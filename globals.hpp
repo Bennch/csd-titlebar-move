14f2ee0d2db4efd4d3caf467463e9f784d84e0e4
Bennch <benniieh@googlemail.com>
2025-10-29 00:05:19 +0100

    Add necessary includes and update global variables
    Added includes for Compositor, KeybindManager, Renderer, and Color. Updated global variables for compositor and keybind manager.

--- globals.hpp
+++ globals.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
+#include <hyprland/src/Compositor.hpp>
+#include <hyprland/src/managers/KeybindManager.hpp>
+#include <hyprland/src/render/Renderer.hpp>
+#include <hyprland/src/helpers/Color.hpp>
 #include <hyprland/src/plugins/PluginAPI.hpp>
-#include <hyprland/src/helpers/memory/Memory.hpp>
-#include <hyprland/src/protocols/XDGShell.hpp>
 
+// Handle für Plugin API
 inline HANDLE PHANDLE = nullptr;
-inline CFunctionHook* g_pCXDGToplevelResConstructor = nullptr;
 
-typedef void (*origCXDGToplevelResConstructor)(CXDGToplevelResource*, SP<CXdgToplevel>, SP<CXDGSurfaceResource>);
-SP<HOOK_CALLBACK_FN> g_pMouseBtnCallback = nullptr;
+// Callback-Objekt für Mausereignisse
+inline SP<HOOK_CALLBACK_FN> g_pMouseBtnCallback = nullptr;
+
+// Zugriff auf globale Manager direkt (werden von Hyprland bereitgestellt)
+extern UP<CCompositor> g_pCompositor;
+extern UP<CKeybindManager> g_pKeybindManager;
